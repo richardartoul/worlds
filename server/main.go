d0220c56c63a8c1d6876606eef72c370809d2478
Richard Artoul <richardartoul@gmail.com>
2017-12-10 22:28:53 -0500

    Add simple webserver to server

--- server/main.go
+++ server/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -11,8 +13,9 @@ import (
 
 func main() {
 	// TODO: Config/Environment variables
-	connectionURL := os.Args[1]
-	contractAddress := os.Args[2]
+	port := os.Args[1]
+	connectionURL := os.Args[2]
+	contractAddress := os.Args[3]
 
 	stateManager := state.NewManager(connectionURL, contractAddress, 5*time.Second)
 	err := stateManager.Init()
@@ -20,10 +23,14 @@ func main() {
 		log.Fatalf("Err initializing state manager: %v", err)
 	}
 
-	for {
-		state := stateManager.Get()
-		fmt.Println("message: ", state.Message)
-		fmt.Println("price: ", state.Price)
-		time.Sleep(5 * time.Second)
+	landingPage, err := template.ParseFiles("templates/landing.html")
+	if err != nil {
+		log.Fatalf("Err parsiing landing page template: %v", err)
 	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		landingPage.Execute(w, stateManager.Get())
+	}
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
