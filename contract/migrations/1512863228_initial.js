315538e628045e1f2e54c95ecfd7e1324d94d0d8
Richard Artoul <richardartoul@gmail.com>
2017-12-10 21:34:10 -0500

    Update pragma and deploy script

--- contract/migrations/1512863228_initial.js
+++ contract/migrations/1512863228_initial.js
@@ -1,4 +1,4 @@
-var SingleMessage = artifacts.require('../contracts/SingleMessage');
+var SingleMessage = artifacts.require('../contracts/SingleMessage.sol');
 
 module.exports = function(deployer, network, accounts) {
   deploy(deployer, accounts);
@@ -6,5 +6,27 @@ module.exports = function(deployer, network, accounts) {
 
 async function deploy(deployer, accounts) {
   const address = accounts[0];
-  await deployer.deploy(SingleMessage, "Hello world!", 1, 200, address);
+  const initialMessage = "Hello world!";
+  const initialPriceInWei = 1;
+  const maxLength = 200;
+
+  console.log("Contract arguments: ", {
+    initialMessage: initialMessage,
+    initialPriceInWei: initialPriceInWei,
+    maxLength: maxLength,
+    address: address
+  });
+
+  return deployer.deploy(SingleMessage, "Hello world!", 1, 200, address).then(async() => {
+    const contract = await SingleMessage.deployed();
+    const message = await contract.message.call();
+    const priceInWei = await contract.priceInWei.call();
+    const maxLength = await contract.maxLength.call();
+
+    console.log("public vars from contract: ", {
+      message: message,
+      priceInWei: priceInWei,
+      maxLength: maxLength
+    });
+  });
 }
