a519989af33c59e72dfbade30a29587c23d9259d
Richard Artoul <richardartoul@gmail.com>
2017-12-12 20:16:46 -0500

    Fix production address

--- contract/truffle.js
+++ contract/truffle.js
@@ -14,9 +14,9 @@ module.exports = {
     production: {
       host: "localhost",
       port: 8543,
-      network_id: "*",
+      network_id: "1",
       gas: 4700000,
-      from: "0x52ce8b05cc9f9bbb6b95c8b501e3d294869372"
+      from: "0x52ce8b05cc449f9bbb6b95c8b501e3d294869372"
     }
   }
 };
